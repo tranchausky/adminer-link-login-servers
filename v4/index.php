b562e7e7cf9c0f8e2cbe59c0cb5e5b07429ac749
chautran <48544755+tranchausky@users.noreply.github.com>
2026-07-06 14:14:50 +0700

    Create index.php

---
+++ v4/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$_ENV['ADMINER_SERVERS'] = json_encode([]);
+
+/*
+$_ENV['ADMINER_SERVERS'] = json_encode([
+    "Local MySQL" => [
+        "driver" => "server",
+        "server" => "127.0.0.1:3306",
+        "username" => "root",
+        "password" => "",
+        "db" => "",
+    ],
+	 "Local MySQL1" => [
+        "driver" => "server",
+        "server" => "127.0.0.1:3306",
+        "username" => "root",
+        "password" => "",
+        "db" => "",
+    ],
+]);
+*/
+
+function adminer_object() {
+    include_once __DIR__ . "/plugins/plugin.php";
+    include_once __DIR__ . "/plugins/login-servers.php";
+
+    return new AdminerPlugin([
+        new AdminerLoginServers(),
+    ]);
+}
+include __DIR__ . "/adminer-4.8.1.php";
+// include __DIR__ . "/adminer-4.17.1.php";
