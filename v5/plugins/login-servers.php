ce46b595b3dcbb59863ddbe2e656aa2f8cdf2257
chautran <48544755+tranchausky@users.noreply.github.com>
2026-07-06 14:16:54 +0700

    Add files via upload

---
+++ v5/plugins/login-servers.php
@@ -0,0 +1,74 @@
+<?php
+
+//include_once __DIR__. '/_config-server.php';
+
+/**
+ * Display servers list from defined ADMINER_SERVERS variable.
+ * @link https://www.adminer.org/plugins/#use
+ * @author https://github.com/garis-space
+*/
+class AdminerLoginServers {
+  /**
+   * Set servers from environment variable
+   * Example:
+   * $_ENV['ADMINER_SERVERS'] = '{
+   *  "Server 1":{"driver":"pgsql","server":"","username":"","password":"","db":""},
+   *  "Server 2":{"driver":"pgsql","server":"","username":"","password":"","db":""}
+   * }';
+  */
+  function __construct() {
+    $this->servers = array();
+	
+    if ($_ENV['ADMINER_SERVERS']) {
+      //$this->servers = json_decode($_ENV['ADMINER_SERVERS'], true);
+	  $list = json_decode($_ENV['ADMINER_SERVERS'], true);
+
+		$list["Server default"] = [
+			"driver" =>"server",
+			"server"=>"192.168.1.104:3336",
+			"username"=>"chau",
+			"password"=>"123456",
+			"db"=>""
+		];
+		
+		//var_dump($list);
+		
+		  $this->servers = $list;
+		  $fileServers = json_decode(
+				file_get_contents(__DIR__ . '/_config-server.json'),
+				true
+			) ?: [];
+		  $this->servers = array_merge($list, $fileServers);
+    }
+
+	if(isset($_POST["auth"]))
+    if ($_POST["auth"]["custom_server"]) {
+      $key = $_POST["auth"]["custom_server"];
+      $_POST["auth"]["driver"] = $this->servers[$key]["driver"];
+      $_POST["auth"]["server"] = $this->servers[$key]["server"];
+      $_POST["auth"]["username"] = $this->servers[$key]["username"];
+      $_POST["auth"]["password"] = $this->servers[$key]["password"];
+      $_POST["auth"]["db"] = $this->servers[$key]["db"];
+    }
+  }
+
+  function loginFormField($name, $heading, $value) {
+    if ($name == 'driver') {
+      return '<tr><th>Driver<td>' . $value;
+    } elseif ($name == 'server') {
+      return '<tr><th>Host<td>' . $value;
+    } elseif ($name == 'db' && $_ENV['ADMINER_SERVERS'] != '') {
+      $out = $heading . $value;
+      $out .= '<tr><th><td>or';
+      $out .= '<tr><th>Server<td><select name="auth[custom_server]">';
+      $out .= '<option value="" selected>--</option>';
+      foreach ($this->servers as $serverName => $serverConfig) {
+          $out .= '<option value="' . htmlspecialchars($serverName) . '">' . htmlspecialchars($serverName) . '</option>';
+      }
+      $out .= '</select>';
+      return $out;
+    }
+  }
+}
+
+return new AdminerLoginServers();
